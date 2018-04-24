7c60869762c9974518fdeb1409f573b46a533b80
Athura <joshualjohnson33@gmail.com>
2018-04-24 16:05:35 -0400

    finished user and began profile

--- devconnect/routes/api/users.js
+++ devconnect/routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load user model
 const User = require("../../models/User");
@@ -78,13 +79,16 @@ router.post("/register", (req, res) => {
 // @description Login user / Returning JWT token
 // @access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find the user by email
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = 'User not found'
+      return res.status(404).json(errors);
     }
 
     // check password, need to decrypt
@@ -106,7 +110,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
