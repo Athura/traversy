414b6be585a82cdf0f8d9a8f8372e726896a166c
Athura <joshualjohnson33@gmail.com>
2018-04-26 08:32:40 -0400

    Profile working

--- devconnect/routes/api/profile.js
+++ devconnect/routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
 // Load profile model
 const Profile = require('../../models/Profile');
 // Load user model
@@ -17,122 +19,156 @@ router.get('/test', (req, res) => res.json({
     msg: 'Profile Works'
 }));
 
-// @route GET api/profile
-// @description Get current users profile
-// @access Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
     const errors = {};
 
-    Profile.findOne({
-        user: req.user.id
-    })
-    .then(profile => {
-        if(!profile){
-            errors.noprofile = 'There is no profile for this user';
-            return res.status(404).json(errors);
+    Profile.findOne({ user: req.user.id })
+      .populate('user', ['name','avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
         }
         res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get(
+  '/all',
+  (req,res) => {
+    const errors = {};
+    Profile.find()
+    .populate('user', ['name','avatar'])
+    .then(profiles => {
+      if(!profiles) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
     })
-    .catch(err => res.status(404).json(err));
-});
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+  }
+);
 
-// @route GET api/profile/test
-// @description Tests profile route
-// @access Public
-router.get('/test', (req, res) => res.json({
-    msg: 'Profile Works'
-}));
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get(
+  '/handle/:handle',
+  (req, res) => {
+    const errors = {};
 
-// @route POST api/profile
-// @description Create user profile
-// @access Private
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const { errors, isValid } = valid
+    Profile.findOne({ handle: req.params.handle })
+      .populate('user', ['name','avatar'])
+      .then(profile => {
+        if(!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          res.status(404).json(errors);
+        }
+
+        //if profile is found
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by userId
+// @access  Public
+router.get(
+  '/user/:user_id',
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.params.user_id })
+      .populate('user', ['name','avatar'])
+      .then(profile => {
+        if(!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          res.status(404).json(errors);
+        }
+
+        //if profile is found
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
+  }
+);
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
 
     // Get fields
     const profileFields = {};
-    // avatar name and email
     profileFields.user = req.user.id;
-
-    //checking to see if this was sent in the request
-    if(req.body.handle){
-        profileFields.handle = req.body.handle;
-    }
-
-    if(req.body.company){
-        profileFields.company = req.body.company;
-    }
-
-    if(req.body.website){
-        profileFields.website = req.body.website;
-    }
-
-    if(req.body.location){
-        profileFields.location = req.body.location;
-    }
-
-    if(req.body.bio){
-        profileFields.bio = req.body.bio;
-    }
-
-    if(req.body.status){
-        profileFields.status = req.body.status;
-    }
-
-    if(req.body.githubusername){
-        profileFields.githubusername = req.body.githubusername;
-    }
-
-    // skills - split into an array. The split gives us an array instead of a list
-    if(typeof req.body.skills !== 'undefined'){
-        profileFields.skills = req.body.skills.split(',');
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    // Skills - Spilt into array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
     }
 
     // Social
     profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
-    if(req.body.youtube){
-        profileFields.social.youtube = req.body.youtube;
-    }
-    if(req.body.twitter){
-        profileFields.social.twitter = req.body.twitter;
-    }
-    if(req.body.facebook){
-        profileFields.social.facebook = req.body.facebook;
-    }
-    if(req.body.linkedin){
-        profileFields.social.linkedin = req.body.linkedin;
-    }
-    if(req.body.instagram){
-        profileFields.social.instagram = req.body.instagram;
-    }
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
 
-    Profile.findOne({ user: req.user.id })
-        .then(profile => {
-            if(profile) {
-                //update the profile
-                Profile.findOneAndUpdate(
-                    { user: req.user.id },
-                    { $set: profileFields },
-                    { new: true }
-                )
-                .then(profile => res.json(profile));
-            } else {
-                //create a profile
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists';
+            res.status(400).json(errors);
+          }
 
-                // Check to see if the handle exists
-                profile.findOne({ handle: profileFields.handle })
-                    .then(profile => {
-                        if(profile) {
-                            errors.handle = 'That handle already exists';
-                            res.status(400).json(errors);
-                        }
-
-                        // If there isnt a profile with the handle then save profile
-                        new Profile(profileFields).save().then(profile => res.json(profile));
-                    })
-            }
-        })
-});
+          // Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
--- devconnect/validation/profile.js
+++ devconnect/validation/profile.js
@@ -1,71 +1,67 @@
-const validator = require('validator');
+const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
 module.exports = function validateProfileInput(data) {
-    let errors = {};
+  let errors = {};
 
-    // This sets empty or undefined values to empty strings
-    data.email = !isEmpty(data.handle) ? data.handle : '';
-    data.status = !isEmpty(data.status) ? data.status : '';
-    data.skills = !isEmpty(data.skills) ? data.skills : '';
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.status = !isEmpty(data.status) ? data.status : '';
+  data.skills = !isEmpty(data.skills) ? data.skills : '';
 
-    // required fields
-    if(!validator.isLength(data.handle, { min: 2, max: 40 })) {
-        errors.handle = 'Handle needs to be between 2 and 4 characters';
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = 'Handle needs to between 2 and 40 characters';
+  }
+
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = 'Profile handle is required';
+  }
+
+  if (Validator.isEmpty(data.status)) {
+    errors.status = 'Status field is required';
+  }
+
+  if (Validator.isEmpty(data.skills)) {
+    errors.skills = 'Skills field is required';
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = 'Not a valid URL';
     }
+  }
 
-    if(validator.isEmpty(data.handle)) {
-        errors.handle = 'Profile handle is required';
+  if (!isEmpty(data.youtube)) {
+    if (!Validator.isURL(data.youtube)) {
+      errors.youtube = 'Not a valid URL';
     }
+  }
 
-    if(validator.isEmpty(data.status)) {
-        errors.status = 'Status field is required';
+  if (!isEmpty(data.twitter)) {
+    if (!Validator.isURL(data.twitter)) {
+      errors.twitter = 'Not a valid URL';
     }
+  }
 
-    if(validator.isEmpty(data.skills)) {
-        errors.skills = 'Skills field is required';
+  if (!isEmpty(data.facebook)) {
+    if (!Validator.isURL(data.facebook)) {
+      errors.facebook = 'Not a valid URL';
     }
+  }
 
-    // not required fields
-    // if website field is not empty then check to see if the provided website is a valid url
-    if(!isEmpty(data.website)){
-        if(!validator.isURL(data.website)) {
-            errors.website = 'Not a valid URL';
-        }
+  if (!isEmpty(data.linkedin)) {
+    if (!Validator.isURL(data.linkedin)) {
+      errors.linkedin = 'Not a valid URL';
     }
+  }
 
-    if(!isEmpty(data.youtube)){
-        if(!validator.isURL(data.youtube)) {
-            errors.youtube = 'Not a valid URL';
-        }
+  if (!isEmpty(data.instagram)) {
+    if (!Validator.isURL(data.instagram)) {
+      errors.instagram = 'Not a valid URL';
     }
+  }
 
-    if(!isEmpty(data.twitter)){
-        if(!validator.isURL(data.twitter)) {
-            errors.twitter = 'Not a valid URL';
-        }
-    }
-
-    if(!isEmpty(data.facebook)){
-        if(!validator.isURL(data.facebook)) {
-            errors.facebook = 'Not a valid URL';
-        }
-    }
-
-    if(!isEmpty(data.linkedin)){
-        if(!validator.isURL(data.linkedin)) {
-            errors.linkedin = 'Not a valid URL';
-        }
-    }
-
-    if(!isEmpty(data.instagram)){
-        if(!validator.isURL(data.instagram)) {
-            errors.instagram = 'Not a valid URL';
-        }
-    }
-
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    };
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
