49b6f9f05762a4b6e1158898fe8bcccbb9213be4
Joshua <joshualjohnson33@gmail.com>
2018-04-29 17:32:14 -0400

    posts

---
+++ devconnect/validation/post.js
@@ -0,0 +1,24 @@
+//needs validator module (npm)
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+    let errors = {};
+
+    data.text = !isEmpty(data.text) ? data.text : '';
+
+    if(!validator.isLength(data.text, { min: 10, max: 300 })) {
+        errors.text = 'Post much be within 10 and 300 characters';
+    }
+
+    if(!validator.isEmail(data.text)) {
+        errors.text = 'Text field is required';
+    }
+
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
