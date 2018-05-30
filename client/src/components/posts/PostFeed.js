fdb63e1e2af80394d2388312b0fa559635e721a6
Athura <joshualjohnson33@gmail.com>
2018-05-30 16:01:22 -0400

    Need to figure out ADD POSTs

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -6,9 +6,7 @@ import PostItem from './PostItem';
   render() {
     const { posts } = this.props;
 
-    return (
-      posts.map(post => <PostItem key={post._id} post={post} />)
-    )
+    return posts.map(post => <PostItem key={post._id} post={post} />)
   }
 }
 
